e8afd601f747121cc35da5a0d6f42b134091e267
sara charafi <elkbircharafi8@gmail.com>
2026-06-12 10:00:51 +0100

    Création de la structure MVC du projet

--- database.sql
+++ database.sql
@@ -1,8 +1,8 @@
--- Base de données PharmaFEFO
+
 CREATE DATABASE IF NOT EXISTS pharmafefo CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 USE pharmafefo;
 
--- Table des utilisateurs
+
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
